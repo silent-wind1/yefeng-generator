01faab925e08a50d643b4d924f6863ff5c08d4b0
yefeng <1797719651@qq.com>
2024-05-15 23:02:25 +0800

    fix(yefeng-generator): 将前端分页参数改成12

--- yefeng-generator-web-frontend/src/pages/Index/index.tsx
+++ yefeng-generator-web-frontend/src/pages/Index/index.tsx
@@ -1,4 +1,7 @@
-import {listGeneratorVoByPageUsingPost} from '@/services/backend/generatorController';
+import {
+  listGeneratorVoByPageFastUsingPost,
+  listGeneratorVoByPageUsingPost
+} from '@/services/backend/generatorController';
 import {UserOutlined} from '@ant-design/icons';
 import {PageContainer, ProFormSelect, ProFormText, QueryFilter} from '@ant-design/pro-components';
 import {Avatar, Card, Flex, Image, Input, List, message, Tabs, Tag, Typography} from 'antd';
@@ -11,7 +14,7 @@ import {Link} from "umi";
  */
 const DEFAULT_PAGE_PARAMS: PageRequest = {
   current: 1,
-  pageSize: 4,
+  pageSize: 12,
   sortField: 'createTime',
   sortOrder: 'descend',
 };
@@ -35,7 +38,7 @@ const IndexPage: React.FC = () => {
   const doSearch = async () => {
     setLoading(true);
     try {
-      const res = await listGeneratorVoByPageUsingPost(searchParams);
+      const res = await listGeneratorVoByPageFastUsingPost(searchParams);
       setDataList(res.data?.records ?? []);
       setTotal(Number(res.data?.total) ?? 0);
     } catch (error: any) {
